a3d65dded525bfc8a1d1bcd3be2667845d8ca3d0
ehsanfeyzolahi <ehsanfeyzolahi@gmail.com>
2018-11-30 14:30:38 +0330

    smooth scroll added

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -21,25 +21,25 @@
                 <img src="{{ asset('img/logotype-websijtje.png') }}" alt="" class="top-log-img">
             </a>
 
-            {{--<div class="collapse navbar-collapse" id="navbarSupportedContent">--}}
-            {{--<ul class="navbar-nav mr-auto" style="margin-left: auto !important; margin-right: 0 !important;">--}}
-            {{--<li class="nav-item">--}}
-            {{--<a class="nav-link" href="#">Who We Are</a>--}}
-            {{--</li>--}}
-            {{--<li class="nav-item">--}}
-            {{--<a class="nav-link" href="#">Packages</a>--}}
-            {{--</li>--}}
-            {{--<li class="nav-item">--}}
-            {{--<a class="nav-link" href="#">Our Works</a>--}}
-            {{--</li>--}}
-            {{--<li class="nav-item">--}}
-            {{--<a class="nav-link" href="#">Team</a>--}}
-            {{--</li>--}}
-            {{--<li class="nav-item">--}}
-            {{--<a class="nav-link" href="#">Contact</a>--}}
-            {{--</li>--}}
-            {{--</ul>--}}
-            {{--</div>--}}
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav mr-auto" style="margin-left: auto !important; margin-right: 0 !important;">
+                    <li class="nav-item">
+                        <a class="nav-link smoothScroll" href="#who_we_are">Who We Are</a>
+                    </li>
+                    {{--<li class="nav-item">--}}
+                    {{--<a class="nav-link" href="#">Packages</a>--}}
+                    {{--</li>--}}
+                    <li class="nav-item">
+                        <a class="nav-link smoothScroll" href="#works">Works</a>
+                    </li>
+                    {{--<li class="nav-item">--}}
+                    {{--<a class="nav-link" href="#">Team</a>--}}
+                    {{--</li>--}}
+                    <li class="nav-item">
+                        <a class="nav-link smoothScroll" href="#contact">Contact</a>
+                    </li>
+                </ul>
+            </div>
         </div>
     </nav>
 </header>
@@ -52,12 +52,12 @@
                 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab aliquid, amet dicta esse molestias nesciunt
                 optio porro quasi ullam. Itaque.
             </p>
-            <button class="btn btn-colored action loader-part">LETS START</button>
+            {{--<button class="btn btn-colored action loader-part">LETS START</button>--}}
         </div>
     </div>
 </section>
 
-<section class="about-us">
+<section class="about-us" id="who_we_are">
     <div class="container">
         <div class="row">
             <div class="col-md-5">
@@ -65,10 +65,14 @@
             </div>
             <div class="col-md-7">
                 <div class="section-text-side loader-part">
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda, commodi corporis debitis
-                    distinctio
-                    earum eligendi exercitationem expedita illo, magni nam obcaecati officiis qui quia velit, veniam.
-                    Assumenda at nisi saepe.
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam consectetur cumque cupiditate
+                    deleniti eaque enim, et hic id illo labore laudantium libero modi non nostrum odio pariatur quis quo
+                    quod repellat rerum? Ad adipisci aliquam asperiores atque corporis dicta eligendi, eveniet ex harum
+                    impedit, magni modi natus optio sapiente suscipit.
+                    <br>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odio officiis omnis, quidem quod rem
+                    repudiandae tenetur vel! Exercitationem maxime pariatur placeat! Accusantium animi deserunt
+                    doloribus ducimus incidunt nostrum reprehenderit similique.
                 </div>
             </div>
         </div>
@@ -99,7 +103,8 @@
                                 <div>
                                     <div class="abilities-title">Websites & Apps</div>
                                     <p class="abilities-description">
-                                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. At facere omnis rerum? Animi
+                                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. At facere omnis rerum?
+                                        Animi
                                         assumenda consequuntur excepturi ipsam minima ratione reiciendis.
                                     </p>
                                 </div>
@@ -122,7 +127,8 @@
                                 <div>
                                     <div class="abilities-title">Graphic Design</div>
                                     <p class="abilities-description">
-                                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet libero, quod? A
+                                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet libero, quod?
+                                        A
                                         adipisci autem ex fugit ipsa natus necessitatibus voluptates?
                                     </p>
                                 </div>
@@ -145,7 +151,8 @@
                                 <div>
                                     <div class="abilities-title">Start-Ups</div>
                                     <p class="abilities-description">
-                                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur consequuntur
+                                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur
+                                        consequuntur
                                         cupiditate eum excepturi harum iusto magni minima officia officiis velit!
                                     </p>
                                 </div>
@@ -168,7 +175,8 @@
                                 <div>
                                     <div class="abilities-title">Branding</div>
                                     <p class="abilities-description">
-                                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur ducimus ex ipsam
+                                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur ducimus ex
+                                        ipsam
                                         iste labore nam omnis repudiandae rerum sunt vero!
                                     </p>
                                 </div>
@@ -182,112 +190,24 @@
     </div>
 </section>
 
-<section class="packages">
+<section class="works" id="works">
     <div class="container">
         <div class="row">
             <div class="col-md-12">
                 <div class="section-title-center">
-                    <span>Packages</span>
+                    <span>Works</span>
                 </div>
             </div>
         </div>
-        {{--<div class="row packages">--}}
-        {{--<div class="col-lg-3 col-md-3 col-sm-3 col-xs-12 package-parent" id="package-1">--}}
-        {{--<div class="box hover">--}}
-        {{--<div class="box-header">--}}
-        {{--<i class="fas fa-angle-left close-package" style="display: none;"></i>--}}
-        {{--</div>--}}
-        {{--<div class="package-price">295</div>--}}
-        {{--<div class="package-description" style="display: none; opacity: 0">--}}
-        {{--<p>--}}
-        {{--Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam, commodi consectetur--}}
-        {{--cupiditate deserunt dolore, earum enim est incidunt inventore ipsum iste labore nihil--}}
-        {{--numquam obcaecati quos reiciendis rerum, sit totam?--}}
-        {{--</p>--}}
-        {{--</div>--}}
-        {{--<div class="box-footer">--}}
-        {{--<i class="fas fa-ellipsis-h open_package_info" data-target="#package-1"></i>--}}
-        {{--</div>--}}
-        {{--</div>--}}
-        {{--<div class="open_package_info" data-target="#package-1" style="cursor: pointer">--}}
-        {{--<div class="package-title">Read more</div>--}}
-        {{--</div>--}}
-        {{--</div>--}}
-
-        {{--<div class="col-lg-3 col-md-3 col-sm-3 col-xs-12 package-parent" id="package-2">--}}
-        {{--<div class="box hover">--}}
-        {{--<div class="box-header">--}}
-        {{--<i class="fas fa-angle-left close-package" style="display: none;"></i>--}}
-        {{--</div>--}}
-        {{--<div class="package-price">450</div>--}}
-        {{--<div class="package-description" style="display: none; opacity: 0">--}}
-        {{--<p>--}}
-        {{--Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam, commodi consectetur--}}
-        {{--cupiditate deserunt dolore, earum enim est incidunt inventore ipsum iste labore nihil--}}
-        {{--numquam obcaecati quos reiciendis rerum, sit totam?--}}
-        {{--</p>--}}
-        {{--</div>--}}
-        {{--<div class="box-footer">--}}
-        {{--<i class="fas fa-ellipsis-h open_package_info" data-target="#package-2"></i>--}}
-        {{--</div>--}}
-        {{--</div>--}}
-        {{--<div class="open_package_info" data-target="#package-2" style="cursor: pointer">--}}
-        {{--<div class="package-title">Read more</div>--}}
-        {{--</div>--}}
-        {{--</div>--}}
-
-        {{--<div class="col-lg-3 col-md-3 col-sm-3 col-xs-12 package-parent" id="package-3">--}}
-        {{--<div class="box hover">--}}
-        {{--<div class="box-header">--}}
-        {{--<i class="fas fa-angle-left close-package" style="display: none;"></i>--}}
-        {{--</div>--}}
-        {{--<div class="package-price">750</div>--}}
-        {{--<div class="package-description" style="display: none; opacity: 0">--}}
-        {{--<p>--}}
-        {{--Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam, commodi consectetur--}}
-        {{--cupiditate deserunt dolore, earum enim est incidunt inventore ipsum iste labore nihil--}}
-        {{--numquam obcaecati quos reiciendis rerum, sit totam?--}}
-        {{--</p>--}}
-        {{--</div>--}}
-        {{--<div class="box-footer">--}}
-        {{--<i class="fas fa-ellipsis-h open_package_info" data-target="#package-3"></i>--}}
-        {{--</div>--}}
-        {{--</div>--}}
-        {{--<div class="open_package_info" data-target="#package-3" style="cursor: pointer">--}}
-        {{--<div class="package-title">Read more</div>--}}
-        {{--</div>--}}
-        {{--</div>--}}
-
-        {{--<div class="col-lg-3 col-md-3 col-sm-3 col-xs-12 package-parent" id="package-4">--}}
-        {{--<div class="box hover">--}}
-        {{--<div class="box-header">--}}
-        {{--<i class="fas fa-angle-left close-package" style="display: none;"></i>--}}
-        {{--</div>--}}
-        {{--<div class="package-price">1000</div>--}}
-        {{--<div class="package-description" style="display: none; opacity: 0">--}}
-        {{--<p>--}}
-        {{--Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam, commodi consectetur--}}
-        {{--cupiditate deserunt dolore, earum enim est incidunt inventore ipsum iste labore nihil--}}
-        {{--numquam obcaecati quos reiciendis rerum, sit totam?--}}
-        {{--</p>--}}
-        {{--</div>--}}
-        {{--<div class="box-footer">--}}
-        {{--<i class="fas fa-ellipsis-h open_package_info" data-target="#package-4"></i>--}}
-        {{--</div>--}}
-        {{--</div>--}}
-        {{--<div class="open_package_info" data-target="#package-4" style="cursor: pointer">--}}
-        {{--<div class="package-title">Read more</div>--}}
-        {{--</div>--}}
-        {{--</div>--}}
-
-        {{--</div>--}}
+        <div class="row">
+        </div>
     </div>
 </section>
 
-<section class="contact">
+<section class="contact" id="contact">
     <div class="container">
         <div class="row">
-            <div class="col-md-5">
+            <div class="col-md-5 loader-part">
                 <div class="section-title-side">Contact</div>
             </div>
             <div class="col-md-7">
@@ -306,19 +226,19 @@
                 {{--</tr>--}}
                 {{--</table>--}}
                 <div class="contact-box">
-                    <a href="">
+                    <a href="" class="loader-part">
                         <i class="fab fa-whatsapp"></i>
                         <span>+98 935 706 2236</span>
                     </a>
-                    <a href="http://t.me/soyEhsan">
+                    <a href="http://t.me/soyEhsan" class="loader-part">
                         <i class="fab fa-telegram"></i>
                         <span>+98 935 706 2236</span>
                     </a>
-                    <a href="">
+                    <a href="" class="loader-part">
                         <i class="fab fa-instagram"></i>
                         <span>@websijtje</span>
                     </a>
-                    <a href="">
+                    <a href="" class="loader-part">
                         <i class="far fa-envelope"></i>
                         <span>contact@websijtje.nl</span>
                     </a>
@@ -334,6 +254,15 @@
         integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
 <script src="{{ asset('bootstrap-4/js/bootstrap.min.js') }}"></script>
 <script>
+    $('a.smoothScroll').click(function (e) {
+        e.preventDefault();
+        var target = $(this).attr('href');
+        var d = $(target).offset().top;
+        $('html,body').animate({
+            scrollTop: d
+        }, 'slow')
+    });
+
     function loadLoaders() {
         var scrolled = $(window).scrollTop();
         var wHeight = $(window).height();
@@ -351,55 +280,6 @@
             }
         });
     }
-
-    function openPackage(packageId) {
-        var el = $(packageId);
-        $('.package-parent').hide();
-        $('.package-parent').css('opacity', '0');
-        el.css('opacity', '1');
-        el.show();
-        el.addClass('active');
-        el.find('.package-description').show();
-        setTimeout(function () {
-            el.find('.package-description').css('opacity', '1');
-            el.find('.box').css({
-                'height': 'auto'
-            });
-        }, 300);
-        el.find('.package-price').hide();
-        el.find('.close-package').show();
-    }
-
-    function closePackages() {
-        //responsive
-        var el = $('.package-parent');
-        el.removeClass('active');
-        el.css({
-            'opacity': '0'
-        });
-        setTimeout(function () {
-            el.show();
-            el.find('.package-description').css('opacity', '0');
-            el.find('.box').css({
-                'height': '200px'
-            });
-            setTimeout(function () {
-                el.css('opacity', '1');
-            }, 100);
-        }, 300);
-        $('.package-description').hide();
-        el.find('.package-price').show();
-        el.find('.close-package').hide();
-
-    }
-
-    $('.open_package_info').click(function () {
-        var target = $(this).data('target');
-        openPackage(target);
-    });
-    $('.close-package').click(function () {
-        closePackages();
-    });
 
     $(document).ready(function () {
         loadLoaders();
