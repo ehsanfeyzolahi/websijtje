e0f5da7db1263f6f93ff71715c789afcdcd4e16a
ehsanfeyzolahi <ehsanfeyzolahi@gmail.com>
2018-11-30 23:22:39 +0330

    tabs bg

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,7 +9,7 @@
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css"
           integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ" crossorigin="anonymous">
     <link rel="stylesheet" href="{{ asset('bootstrap-4/css/bootstrap.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}?v=0.1.3">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}?v=0.1.5">
     <title>websijtje</title>
 </head>
 <body>
