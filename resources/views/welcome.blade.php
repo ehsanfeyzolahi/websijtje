aaf0978ce573d13ca829654701c29561a900be47
ehsanfeyzolahi <ehsanfeyzolahi@gmail.com>
2019-05-25 10:51:13 +0430

    some minor changes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -14,7 +14,7 @@
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css"
           integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ" crossorigin="anonymous">
     <link rel="stylesheet" href="{{ asset('bootstrap-4/css/bootstrap.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}?v=0.2.0">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}?v=0.3.0">
     <title>websijtje</title>
 </head>
 <body>
@@ -23,7 +23,8 @@
     <nav class="navbar navbar-expand-lg ">
         <div class="container">
             <a class="navbar-brand" href="{{ url('/') }}">
-                <img src="{{ asset('img/logotype-websijtje.png') }}" alt="" class="top-log-img">
+                {{--<img src="{{ asset('img/logotype-websijtje.png') }}" alt="" class="top-log-img">--}}
+                Websijtje
             </a>
 
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
@@ -34,9 +35,9 @@
                     <li class="nav-item">
                         <a class="nav-link smoothScroll" href="#packages">Packages</a>
                     </li>
-                    <li class="nav-item">
-                        <a class="nav-link smoothScroll" href="#works">Works</a>
-                    </li>
+                    {{--<li class="nav-item">--}}
+                        {{--<a class="nav-link smoothScroll" href="#works">Works</a>--}}
+                    {{--</li>--}}
                     {{--<li class="nav-item">--}}
                     {{--<a class="nav-link" href="#">Team</a>--}}
                     {{--</li>--}}
@@ -56,9 +57,9 @@
                 <div class="welcome-box">
                     <div class="title loader-part">HELLO.</div>
                     <p class="loader-part">
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab aliquid, amet dicta esse molestias
-                        nesciunt
-                        optio porro quasi ullam. Itaque.
+                        You are not middle of the road, . You want to stand out and we can help you. We make nice
+                        tailored websites that meet your needs and wishes. For a nice and unbeatable price. Why? Because
+                        you deserve it.
                     </p>
                     {{--<button class="btn btn-colored action loader-part">LETS START</button>--}}
                 </div>
@@ -81,7 +82,7 @@
                     </div>
                 </div>
                 <div class="cover-slide second">
-                    <div class="title"></div>
+                    <div class="title">Start-Ups</div>
                     <div class="icon">
                         <img src="{{ asset('img/icons/web_app.png') }}" alt="">
                     </div>
@@ -129,10 +130,10 @@
                     </div>
                 </div>
             </div>
-            <div class="cover-slide-controller">
-                <div class="control prev"><</div>
-                <div class="control next">></div>
-            </div>
+            {{--<div class="cover-slide-controller">--}}
+            {{--<div class="control prev"><</div>--}}
+            {{--<div class="control next">></div>--}}
+            {{--</div>--}}
         </div>
     </div>
 </section>
@@ -145,14 +146,14 @@
             </div>
             <div class="col-md-7">
                 <div class="section-text-side loader-part">
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam consectetur cumque cupiditate
-                    deleniti eaque enim, et hic id illo labore laudantium libero modi non nostrum odio pariatur quis quo
-                    quod repellat rerum? Ad adipisci aliquam asperiores atque corporis dicta eligendi, eveniet ex harum
-                    impedit, magni modi natus optio sapiente suscipit.
+                    <a href="{{ url('/') }}">Websijtje.nl</a> is a Dutch/Iranian company. Founded after a 10 minute encounter in the metro of Tehran
+                    in September 2017.
                     <br>
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odio officiis omnis, quidem quod rem
-                    repudiandae tenetur vel! Exercitationem maxime pariatur placeat! Accusantium animi deserunt
-                    doloribus ducimus incidunt nostrum reprehenderit similique.
+                    We will make your website according to your wishes.
+                    For a very competitive and unbeatable price. we are of course familiar with UX and
+                    UI.
+                    <br>
+                    Your website will be hosted in Amsterdam.
                 </div>
             </div>
         </div>
@@ -274,122 +275,122 @@
     </div>
 </section>
 
-<section class="works" id="works">
-    <div class="container loader-part">
-        <div class="row">
-            <div class="col-md-12">
-                <div class="section-title-center">
-                    <span>Works</span>
-                </div>
-            </div>
-        </div>
-        <div class="row works-container">
-            <div class="container">
-                <ul class="nav nav-tabs work-tabs" id="myTab" role="tablist">
-                    <li class="nav-item">
-                        <a class="nav-link active" id="home-tab" data-toggle="tab" href="#websites" role="tab"
-                           aria-controls="websites" aria-selected="true">Websites</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" id="profile-tab" data-toggle="tab" href="#profile" role="tab"
-                           aria-controls="profile" aria-selected="false">Design</a>
-                    </li>
-                </ul>
-            </div>
-            <div class="tab-content works-tabs-parts" id="myTabContent" style="width: 100%">
-                <div class="tab-pane fade show active" id="websites" role="tabpanel" aria-labelledby="home-tab"
-                     style="width: 100%">
-                    <div class="row">
+{{--<section class="works" id="works">--}}
+    {{--<div class="container loader-part">--}}
+        {{--<div class="row">--}}
+            {{--<div class="col-md-12">--}}
+                {{--<div class="section-title-center">--}}
+                    {{--<span>Works</span>--}}
+                {{--</div>--}}
+            {{--</div>--}}
+        {{--</div>--}}
+        {{--<div class="row works-container">--}}
+            {{--<div class="container">--}}
+                {{--<ul class="nav nav-tabs work-tabs" id="myTab" role="tablist">--}}
+                    {{--<li class="nav-item">--}}
+                        {{--<a class="nav-link active" id="home-tab" data-toggle="tab" href="#websites" role="tab"--}}
+                           {{--aria-controls="websites" aria-selected="true">Websites</a>--}}
+                    {{--</li>--}}
+                    {{--<li class="nav-item">--}}
+                        {{--<a class="nav-link" id="profile-tab" data-toggle="tab" href="#profile" role="tab"--}}
+                           {{--aria-controls="profile" aria-selected="false">Design</a>--}}
+                    {{--</li>--}}
+                {{--</ul>--}}
+            {{--</div>--}}
+            {{--<div class="tab-content works-tabs-parts" id="myTabContent" style="width: 100%">--}}
+                {{--<div class="tab-pane fade show active" id="websites" role="tabpanel" aria-labelledby="home-tab"--}}
+                     {{--style="width: 100%">--}}
+                    {{--<div class="row">--}}
 
-                        <div class="col-lg-4 col-md-6 col-sm-12 col-sm-12 loader-part">
-                            <a href="#">
-                                <div class="work-box website">
-                                    <div class="work-box-bg"
-                                         style="background-image: url('{{ asset('img/websites/1.jpg') }}')"></div>
-                                    <div class="work-box-header"><i class="far fa-ellipsis-h"></i></div>
-                                    <div class="work-info">
-                                        <div>
-                                            <div class="project-title">websijtje</div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </a>
-                        </div>
+                        {{--<div class="col-lg-4 col-md-6 col-sm-12 col-sm-12 loader-part">--}}
+                            {{--<a href="#">--}}
+                                {{--<div class="work-box website">--}}
+                                    {{--<div class="work-box-bg"--}}
+                                         {{--style="background-image: url('{{ asset('img/websites/1.jpg') }}')"></div>--}}
+                                    {{--<div class="work-box-header"><i class="far fa-ellipsis-h"></i></div>--}}
+                                    {{--<div class="work-info">--}}
+                                        {{--<div>--}}
+                                            {{--<div class="project-title">websijtje</div>--}}
+                                        {{--</div>--}}
+                                    {{--</div>--}}
+                                {{--</div>--}}
+                            {{--</a>--}}
+                        {{--</div>--}}
 
-                        <div class="col-lg-4 col-md-6 col-sm-12 col-sm-12 loader-part">
-                            <a href="#">
-                                <div class="work-box website">
-                                    <div class="work-box-bg"
-                                         style="background-image: url('{{ asset('img/websites/2.jpg') }}')"></div>
-                                    <div class="work-box-header"><i class="far fa-ellipsis-h"></i></div>
-                                    <div class="work-info">
-                                        <div>
-                                            <div class="project-title">websijtje</div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </a>
-                        </div>
-                    </div>
-                </div>
-                <div class="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab"
-                     style="width: 100%">
-                    <div class="row">
+                        {{--<div class="col-lg-4 col-md-6 col-sm-12 col-sm-12 loader-part">--}}
+                            {{--<a href="#">--}}
+                                {{--<div class="work-box website">--}}
+                                    {{--<div class="work-box-bg"--}}
+                                         {{--style="background-image: url('{{ asset('img/websites/2.jpg') }}')"></div>--}}
+                                    {{--<div class="work-box-header"><i class="far fa-ellipsis-h"></i></div>--}}
+                                    {{--<div class="work-info">--}}
+                                        {{--<div>--}}
+                                            {{--<div class="project-title">websijtje</div>--}}
+                                        {{--</div>--}}
+                                    {{--</div>--}}
+                                {{--</div>--}}
+                            {{--</a>--}}
+                        {{--</div>--}}
+                    {{--</div>--}}
+                {{--</div>--}}
+                {{--<div class="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab"--}}
+                     {{--style="width: 100%">--}}
+                    {{--<div class="row">--}}
 
-                        <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12">
-                            <div class="work-box design">
-                                <div class="work-box-bg"
-                                     style="background-image: url('{{ asset('img/designs/1.jpeg') }}')"></div>
-                                <div class="work-info">
-                                    <div>
-                                        <div class="project-title">websijtje</div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
+                        {{--<div class="col-lg-4 col-md-4 col-sm-6 col-xs-12">--}}
+                            {{--<div class="work-box design">--}}
+                                {{--<div class="work-box-bg"--}}
+                                     {{--style="background-image: url('{{ asset('img/designs/1.jpeg') }}')"></div>--}}
+                                {{--<div class="work-info">--}}
+                                    {{--<div>--}}
+                                        {{--<div class="project-title">websijtje</div>--}}
+                                    {{--</div>--}}
+                                {{--</div>--}}
+                            {{--</div>--}}
+                        {{--</div>--}}
 
-                        <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12">
-                            <div class="work-box design">
-                                <div class="work-box-bg"
-                                     style="background-image: url('{{ asset('img/designs/2.jpeg') }}')"></div>
-                                <div class="work-info">
-                                    <div>
-                                        <div class="project-title">websijtje</div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
+                        {{--<div class="col-lg-4 col-md-4 col-sm-6 col-xs-12">--}}
+                            {{--<div class="work-box design">--}}
+                                {{--<div class="work-box-bg"--}}
+                                     {{--style="background-image: url('{{ asset('img/designs/2.jpeg') }}')"></div>--}}
+                                {{--<div class="work-info">--}}
+                                    {{--<div>--}}
+                                        {{--<div class="project-title">websijtje</div>--}}
+                                    {{--</div>--}}
+                                {{--</div>--}}
+                            {{--</div>--}}
+                        {{--</div>--}}
 
-                        <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12">
-                            <div class="work-box design">
-                                <div class="work-box-bg"
-                                     style="background-image: url('{{ asset('img/designs/3.jpeg') }}')"></div>
-                                <div class="work-info">
-                                    <div>
-                                        <div class="project-title">websijtje</div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
+                        {{--<div class="col-lg-4 col-md-4 col-sm-6 col-xs-12">--}}
+                            {{--<div class="work-box design">--}}
+                                {{--<div class="work-box-bg"--}}
+                                     {{--style="background-image: url('{{ asset('img/designs/3.jpeg') }}')"></div>--}}
+                                {{--<div class="work-info">--}}
+                                    {{--<div>--}}
+                                        {{--<div class="project-title">websijtje</div>--}}
+                                    {{--</div>--}}
+                                {{--</div>--}}
+                            {{--</div>--}}
+                        {{--</div>--}}
 
-                        <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12">
-                            <div class="work-box design">
-                                <div class="work-box-bg"
-                                     style="background-image: url('{{ asset('img/designs/4.jpeg') }}')"></div>
-                                <div class="work-info">
-                                    <div>
-                                        <div class="project-title">websijtje</div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
+                        {{--<div class="col-lg-4 col-md-4 col-sm-6 col-xs-12">--}}
+                            {{--<div class="work-box design">--}}
+                                {{--<div class="work-box-bg"--}}
+                                     {{--style="background-image: url('{{ asset('img/designs/4.jpeg') }}')"></div>--}}
+                                {{--<div class="work-info">--}}
+                                    {{--<div>--}}
+                                        {{--<div class="project-title">websijtje</div>--}}
+                                    {{--</div>--}}
+                                {{--</div>--}}
+                            {{--</div>--}}
+                        {{--</div>--}}
 
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</section>
+                    {{--</div>--}}
+                {{--</div>--}}
+            {{--</div>--}}
+        {{--</div>--}}
+    {{--</div>--}}
+{{--</section>--}}
 
 <section class="packages" id="packages">
     <div class="container">
@@ -557,9 +558,7 @@
             <div class="col-md-1"></div>
             <div class="col-md-10 text-center">
                 <p class="text-center loader-part packages-description">
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam beatae, debitis deserunt
-                    distinctio dolores doloribus eaque eligendi eos est eum eveniet iure, minus nostrum perspiciatis
-                    quaerat quas, quasi saepe totam.
+                    To make it easy for you we developed four packages. Each package has also three options. Easy and clear.
                 </p>
                 <button class="btn btn-colored action loader-part">LETS START</button>
             </div>
@@ -594,15 +593,15 @@
                         <i class="fab fa-whatsapp"></i>
                         <span>+98 935 706 2236</span>
                     </a>
-                    <a href="http://t.me/soyEhsan" class="loader-part">
-                        <i class="fab fa-telegram"></i>
-                        <span>+98 935 706 2236</span>
-                    </a>
-                    <a href="" class="loader-part">
+                    {{--<a href="http://t.me/soyEhsan" class="loader-part">--}}
+                        {{--<i class="fab fa-telegram"></i>--}}
+                        {{--<span>+98 935 706 2236</span>--}}
+                    {{--</a>--}}
+                    <a href="http://instagram.com/websijtje" class="loader-part" target="_blank">
                         <i class="fab fa-instagram"></i>
                         <span>@websijtje</span>
                     </a>
-                    <a href="" class="loader-part">
+                    <a href="mailto:contact@websijtje.nl" class="loader-part" target="_blank">
                         <i class="far fa-envelope"></i>
                         <span>contact@websijtje.nl</span>
                     </a>
